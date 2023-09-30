7e6b7fe4bbb98bde068c7d8b8b9597b1e6408b24
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-30 20:48:50 +0200

    Show dropdown with button

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,13 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ['divToToggle']
-  static classes = ['hidden']
+  static targets = ['divToToggle', 'dropDownToShow']
+  static classes = ['hidden', 'visible']
 
   connect() {
   }
 
   toggleVisibility() {
     this.divToToggleTarget.classList.toggle(this.hiddenClass)
+  }
+
+  hide() {
+
+  }
+
+  show() {
+    this.dropDownToShowTarget.className = this.visibleClass
   }
 }
