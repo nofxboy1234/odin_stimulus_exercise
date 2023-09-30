454294967b42f55ec5868f13a06ba8d656ef7165
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-30 21:11:12 +0200

    Hide a button and show another

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,22 +1,29 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ['divToToggle', 'dropDownToShow']
-  static classes = ['hidden', 'visible']
+  static targets = [
+    'divToToggle',
+    'dropDownToShow',
+    'buttonToHide',
+    'buttonToShow',
+  ];
+  static classes = ['hidden', 'visible'];
 
-  connect() {
-  }
+  connect() {}
 
   toggleVisibility() {
-    this.divToToggleTarget.classList.toggle(this.hiddenClass)
+    this.divToToggleTarget.classList.toggle(this.hiddenClass);
   }
 
-  hide() {
-
+  hideThenShow() {
+    this.buttonToHideTarget.className = this.hiddenClass;
+    this.buttonToShowTarget.className = this.visibleClass;
   }
 
-  show() {
-    this.dropDownToShowTarget.className = this.visibleClass
+  show(event, element = null) {
+    this.dropDownToShowTarget.className = this.visibleClass;
+    // if (element) {
+    // }
   }
 }
