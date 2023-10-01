6da96dabfb7e2de6dd403a5b81a2a7a41046f490
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-01 10:48:11 +0200

    Warn when tweet is too long

--- app/javascript/controllers/tweet_controller.js
+++ app/javascript/controllers/tweet_controller.js
@@ -2,10 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="tweet"
 export default class extends Controller {
+  static targets = ['warning']
+  static classes = ['hidden', 'visible', 'warning']  
+
   connect() {
   }
 
   checkLength(event) {
     console.log(event.target.value.length)
+
+    if (event.target.value.length > 10) {
+      this.warningTarget.className = this.visibleClass
+      this.warningTarget.classList.add(this.warningClass)
+    }
+    else {
+      this.warningTarget.className = this.hiddenClass
+    }
   }
+
 }
